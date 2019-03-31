efb0569063646677623215523323727574c30cf7
Antoine <solnichkin.antoine@gmail.com>
2019-03-31 19:59:42 +0200

    Auth logic when logging with Google

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -10,8 +13,17 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refresh token", refreshToken);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // Already have a record with this profile id.
+          done(null, existingUser);
+        } else {
+          // Creating a new user.
+          new User({ googleId: profile.id }).save().then(user => {
+            done(null, user);
+          });
+        }
+      });
     }
   )
 );
